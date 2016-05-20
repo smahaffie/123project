c2edc82c43a14398f8e2631d3c4c14d85a8fe2c9
CasperN <casperneo@uchicago.edu>
2016-05-19 22:03:04 -0500

    supercat

---
+++ data_cleaning/supercat.sh
@@ -0,0 +1,6 @@
+
+cp  /mnt/census/census_2000/datasets/Summary_File_1/*/*01_uf1.zip  ~/thepile                   \
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*02_uf3.zip  ~/thepile             
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*03_uf3.zip  ~/thepile         
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*05_uf3.zip  ~/thepile        
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*06_uf3.zip  ~/thepile
