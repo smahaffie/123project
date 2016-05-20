01d93c19dc06cefdf4bdd689df9659996195c6e9
Shelby Mahaffie <shelbymahaffie@wireless-s0-no-150-0-240.uchicago.edu>
2016-05-20 11:31:04 -0500

    a

--- cleaners/generate_sample_stats.py
+++ cleaners/generate_sample_stats.py
@@ -2,54 +2,58 @@ import csv
 import re
 import math
 
-def create_summary_files():
-    with open("sample_vectors.txt", 'r') as f:
-        variables = f.readline().strip().split(',')[1:]
+def create_summary_files(raw_file, data_file, avgs_file, stds_file):
+    with open(raw_file, 'r') as f:
+        add_variables = f.readline().strip().split(',')[3:]
+        variables = ['lat', 'lon']
+        for var in add_variables:
+            var = var.replace('"','').strip()
+            variables.append(var)
 
         data = []
         places = []
         for line in f:
-            place = line.split('"')[1]
-            places.append(place[:-1])
-            line = line.split('"')[3].split(",")
-            data.append(line)
+            place = line.split('"')[1].split(",")[0]
+            places.append(place)
+            data_on_line = line.split('"')[1].split(",")[1:3] + line.split('"')[3].split(",")
+            if data_on_line[0] != "lat":
+                data.append(data_on_line)
 
     means = []
     stds = []
-    for i in range(len(variables)):
+    for i in range(len(variables)-2):
         tot = 0
         for line in data:
-            if not line[i] == "missing":
-                tot += float(line[i])
+
+            if not line[i+2] == "missing":
+                tot += float(line[i+2])
 
         mean = tot/len(data)
         means.append("{0:.4f}".format(mean))
 
         squared_deviations = 0
         for line in data:
-            if not line[i] == "missing":
-                squared_deviations += (float(line[i])-mean) ** 2
+            if not line[i+2] == "missing":
+                squared_deviations += (float(line[i+2])-mean) ** 2
 
         std = math.sqrt(squared_deviations/len(data))
 
         stds.append("{0:.4f}".format(std))
 
-    with open("sample_averages.csv", "w") as f:
-        f.write(",".join(variables) + "\n")
+    with open(avgs_file, "w") as f:
+        f.write(",".join(variables[2:]) + "\n")
         f.write(",".join(means) + "\n")
 
-    with open("sample_stds.csv", "w") as f:
-        f.write(",".join(variables) + "\n")
+    with open(stds_file, "w") as f:
+        f.write(",".join(variables[2:]) + "\n")
         f.write(",".join(stds) + "\n")
 
-    with open("sample_data.csv", "w") as f:
+    with open(data_file, "w") as f:
         f.write("place," + ",".join(variables) + "\n")
         i = 0
         for line in data:
             f.write(places[i] + "," + ",".join(line) + "\n")
             i += 1
-
-
 
 
             
