67b3bebee6cf416d6da2087539b8734ca02251d3
CasperN <casperneo@uchicago.edu>
2016-05-26 15:09:53 -0500

    lol

--- bash scripts and conf/remakevolume.sh
+++ bash scripts and conf/remakevolume.sh
@@ -1,14 +1,14 @@
 
 
-mkdir /mnt/123/zipped
-mkdir /mnt/123/unzipped
+mkdir /mnt/volume/zipped
+mkdir /mnt/volume/unzipped
 
-cp  /mnt/census/census_2000/datasets/Summary_File_1/*/*01_uf1.zip  /mnt/123/zipped       
-cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*02_uf3.zip  /mnt/123/zipped       
-cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*03_uf3.zip  /mnt/123/zipped       
-cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*05_uf3.zip  /mnt/123/zipped       
-cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*06_uf3.zip  /mnt/123/zipped
+cp  /mnt/census/census_2000/datasets/Summary_File_1/*/*01_uf1.zip  /mnt/volume/zipped       
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*02_uf3.zip  /mnt/volume/zipped       
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*03_uf3.zip  /mnt/volume/zipped       
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*05_uf3.zip  /mnt/volume/zipped       
+cp  /mnt/census/census_2000/datasets/Summary_File_3/*/*06_uf3.zip  /mnt/volume/zipped
 
-unzip "/mnt/123/zipped/*.zip" -d /mnt/123/unzipped
-cat /mnt/123/unzipped/*geo* > /mnt/123/supergeo.txt
-cat /mnt/123/unzipped/*0*.u* > ~/mnt/123/superfile.txt
+unzip "/mnt/volume/zipped/*.zip" -d /mnt/123/unzipped
+cat /mnt/volume/unzipped/*geo* > /mnt/123/supergeo.txt
+cat /mnt/volume/unzipped/*0*.u* > ~/mnt/123/superfile.txt
