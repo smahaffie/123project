959d6998a0f05ecb0c0edc4b2c192e1071939ead
sherryshenker <sshenker@uchicago.edu>
2016-05-28 17:58:52 -0500

    mapping code and block level header filter

--- cleaners/header_with_lat_long_census_tracts.py
+++ cleaners/header_with_lat_long_census_tracts.py
@@ -7,7 +7,7 @@ def find_lonlat( line):
 	'''
 	Find the geographic location of a line in the header file of the census data
 	'''
-	
+
 	regex = "[\+,-][0-9]{8}"
 	regex = "[\+,-][0-9]{8,11}"
 	res = re.findall(regex,line)
@@ -42,7 +42,7 @@ def find_index_places(file,state):
 			code = l[3][:5]
 			sum_file = l[0]
 			name_approx = line[195:220]
-			if l[1][2:5] == "158":
+			if l[1][2:5] == "150":
 				name = []
 				for i in name_approx:
 					if not i.isdigit() and i != " ":
@@ -51,7 +51,7 @@ def find_index_places(file,state):
 				list_codes.append([counter,name,round(lat,4),round(lon,4)])
 			counter += 1
 
-	with open("{}_{}_tracts.csv".format(state,sum_file),'w') as f:
+	with open("{}_{}_block.csv".format(state,sum_file),'w') as f:
 		writer = csv.writer(f)
 		writer.writerows(list_codes)
 
@@ -63,4 +63,4 @@ if __name__=="__main__":
 		"ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT",
 		"VA","WA","WV", "WI", "WY"]:
 		find_index_places("{}geo.uf1".format(state.lower()),state)
-		find_index_places("{}geo.uf3".format(state.lower()),state)
+		find_index_places("{}geo.uf3".format(state.lower()),state)'''
