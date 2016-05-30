e2e6c732c6cb30e5534455dc6871c2177708e7f1
CasperN <casperneo@uchicago.edu>
2016-05-30 10:42:04 -0500

    .

--- analyzers/springs stuff/MPI_handler.py
+++ analyzers/springs stuff/MPI_handler.py
@@ -2,25 +2,31 @@ import os
 import sys
 from multiprocessing import Pool
 
+
+"""
+THIS FILE WRITES THE INSTRUCTIONS FOR SETTING UP MULTIPLE
+"""
+
+
 SPRINGSPATH = "springs.py"
 PEMPATH = "~/laptop.pem"
 NO_STRICT_KEY = "-o 'StrictHostKeyChecking no'"
 
 
-BIG_OL_LIST = [
-                ('172.31.58.163', 'ec2-54-85-163-133.compute-1.amazonaws.com'), # node 0
-                ('172.31.57.70', 'ec2-54-84-232-149.compute-1.amazonaws.com'),
-                ('172.31.57.69', 'ec2-54-85-171-19.compute-1.amazonaws.com'),
-                ('172.31.57.68', 'ec2-54-86-44-218.compute-1.amazonaws.com'),
-                ('172.31.57.67', 'ec2-54-172-189-241.compute-1.amazonaws.com'),
-                ('172.31.57.71', 'ec2-54-174-149-115.compute-1.amazonaws.com'),
+IP_DNS_LIST = [
+                ('172.31.48.191', 'ec2-52-90-136-20.compute-1.amazonaws.com'), # node 0
+                ('172.31.48.189', 'ec2-54-85-253-140.compute-1.amazonaws.com'),
+                ('172.31.48.192', 'ec2-54-89-105-133.compute-1.amazonaws.com'),
+                ('172.31.48.190', 'ec2-54-174-186-93.compute-1.amazonaws.com'),
+                ('172.31.48.187', 'ec2-54-175-177-251.compute-1.amazonaws.com'),
+                ('172.31.48.188', 'ec2-54-175-234-57.compute-1.amazonaws.com'),
             ]
 
 def write_superbash():
 
     f = open('mpi_setup.sh','w')
     echo = ''
-    for ip, dns in BIG_OL_LIST[1:]:
+    for ip, dns in IP_DNS_LIST[1:]:
         echo += 'echo {} >> hosts \n'.format(ip)
 
 
@@ -32,7 +38,8 @@ source .bashrc
 
 {} 
 sudo pip install numpy 
-wget https://pypi.python.org/packages/source/m/mpi4py/mpi4py-1.3.1.tar.gz 
+sudo pip install matplotlib
+wget https://pypi.python.org/packages/source/m/mpi4py/mpi4pLSy-1.3.1.tar.gz 
 tar xzf mpi4py-1.3.1.tar.gz 
 cd mpi4py-1.3.1 
 python setup.py build 
@@ -43,9 +50,9 @@ echo 'done here'
 
     f.write(here)
 
-    for ip,dns in BIG_OL_LIST[1:]:
+    for ip,dns in IP_DNS_LIST[1:]:
         echo = ''
-        for ip2,dns2 in BIG_OL_LIST:
+        for ip2,dns2 in IP_DNS_LIST:
             if ip != ip2:
                 echo += 'ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "echo {2} >> hosts" \n'.format(PEMPATH,dns,ip2)
         there ="""
@@ -56,6 +63,7 @@ scp -i {0} -o StrictHostKeyChecking=no {3} ec2-user@{1}:~
 ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "sudo easy_install pip" 
 ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "sudo easy_install --upgrade pip" 
 ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "sudo /usr/local/bin/pip install numpy" 
+ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "sudo /usr/local/bin/pip install matplotlib" 
 ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "sudo yum install mpich-devel"
 ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "echo export PATH=/usr/lib64/mpich/bin/:$PATH >> .bashrc" 
 ssh -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "echo export LD_LIBRARY_PATH=/usr/lib64/mpich/lib/:$LD_LIBRARY_PATHPATH >> .bashrc" 
@@ -73,8 +81,8 @@ echo 'done {1}'
 
     #cross ssh
     f.write("\necho 'cross ssh'\n")
-    for ip,dns in BIG_OL_LIST:
-        for ip2,dns2 in BIG_OL_LIST:
+    for ip,dns in IP_DNS_LIST:
+        for ip2,dns2 in IP_DNS_LIST:
             if ip != ip2:
                 s = '''ssh -t -i {0} -o StrictHostKeyChecking=no ec2-user@{1} "ssh {2}"\n'''.format(PEMPATH,dns,ip2)
                 f.write(s)
