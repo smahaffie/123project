96f5a8a9ba63d30253fd5072d429fca97b961d1c
sherryshenker <sshenker@uchicago.edu>
2016-06-01 18:26:10 -0500

    Update homogenous_MR.py

--- analyzers/homogenous_MR.py
+++ analyzers/homogenous_MR.py
@@ -51,7 +51,7 @@ class make_graph(mrj):
             a_n     =  active_nodes.pop()   # active node
 
             for n in self.neighbors.get(a_n,[]):    #catch no neighbor exception
-                d = self.difference(n, origin)**4    # increase cost of extra distance
+                d = self.difference(n, origin)**6    # increase cost of extra distance
                 this_path = G.node[a_n]['shortest_path'] + d
 
                 if this_path < self.epsilon:
@@ -72,7 +72,7 @@ class make_graph(mrj):
         '''
         self.vectors = json.load(open(self.options.vectors))
         self.neighbors = json.load(open(self.options.neighbors))
-        self.epsilon = 7
+        self.epsilon = 12
 
     def mapper(self,_,line):
         G = self.dykstra(line)
